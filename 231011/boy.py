f89ce176ed135eb3db4eba8022f823978b1d3a6c
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-10-11 17:52:22 +0900

    오.. 움직이고 있어 요

--- 231011/boy.py
+++ 231011/boy.py
@@ -7,35 +7,38 @@ from pico2d import load_image
 class Idle:
 
     @staticmethod
-    def enter():
-        print('Idle Enter')
+    def enter(boy):
+        print('Idle Enter - 고개 숙이기')
 
     @staticmethod
-    def exit():
-        print('Idle Exit')
+    def exit(boy):
+        print('Idle Exit - 고개 들기')
 
     @staticmethod
-    def do():
-        print('Idle Do')
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        print('Idle Do - 드르렁')
 
     @staticmethod
-    def draw():
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
         pass
 
 
 
 class StateMachine:
-    def __init__(self):
+    def __init__(self, boy): # self는 statemachine 자신, boy가 얘 생성할때 넘겨주는 첫번째 인자인 것임.
         self.cur_state = Idle
+        self.boy = boy
 
     def start(self):
-        self.cur_state.enter()
+        self.cur_state.enter(self.boy)
 
     def update(self):
-        self.cur_state.do()
+        self.cur_state.do(self.boy)
 
     def draw(self):
-        self.cur_state.draw()
+        self.cur_state.draw(self.boy)
 
 
 
@@ -47,7 +50,7 @@ class Boy:
         self.frame = 0
         self.action = 3
         self.image = load_image('animation_sheet.png')
-        self.state_machine = StateMachine()
+        self.state_machine = StateMachine(self)
         self.state_machine.start()
 
     def update(self):
