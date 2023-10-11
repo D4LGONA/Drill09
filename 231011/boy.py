fc718f14a25599c5f42faccc524cc2f18e58e338
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-10-11 18:33:27 +0900

    Update boy.py

--- 231011/boy.py
+++ 231011/boy.py
@@ -1,14 +1,22 @@
 # 이것은 각 상태들을 객체로 구현한 것임.
 
-from pico2d import load_image
+from pico2d import load_image, SDL_KEYDOWN, SDLK_SPACE, get_time
+from math import *
 
 
+#define event check functions
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
 
 class Idle:
 
     @staticmethod
     def enter(boy):
         print('Idle Enter - 고개 숙이기')
+        boy.idle_start_time = get_time()
 
     @staticmethod
     def exit(boy):
@@ -17,19 +25,52 @@ class Idle:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
+        if get_time() - boy.idle_start_time > 3:
+            boy.state_machine.handle_event(('TIME_OUT', 0))
         print('Idle Do - 드르렁')
 
     @staticmethod
     def draw(boy):
         boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+class Sleep:
+
+    @staticmethod
+    def enter(boy):
         pass
+
+    @staticmethod
+    def exit(boy):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_composite_draw(boy.frame * 100, boy.action * 100, 100, 100, pi / 2, '', boy.x - 25, boy.y - 25, 100, 100)
 
 
 
 class StateMachine:
     def __init__(self, boy): # self는 statemachine 자신, boy가 얘 생성할때 넘겨주는 첫번째 인자인 것임.
-        self.cur_state = Idle
+        self.cur_state = Sleep
         self.boy = boy
+        self.transitions = {
+            Sleep: {space_down: Idle}, # 이중 딕 셔너리
+            Idle: {time_out: Sleep}
+        }
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.boy)
+                self.cur_state = next_state
+                self.cur_state.enter(self.boy)
+                return True
+        return False
+
 
     def start(self):
         self.cur_state.enter(self.boy)
@@ -57,6 +98,7 @@ class Boy:
         self.state_machine.update()
 
     def handle_event(self, event):
+        self.state_machine.handle_event(('INPUT', event))
         pass
 
     def draw(self):
